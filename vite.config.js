eca73e3accf88175494bc1821c7db17d9b51e338
MohammadAsif34 <shadowforever34@gmail.com>
2026-02-05 23:27:50 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,21 +3,36 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
+
+      // 🔥 REQUIRED FOR LOCALHOST INSTALL
+      devOptions: {
+        enabled: true,
+      },
+
       manifest: {
         name: "My React App",
         short_name: "ReactApp",
+        start_url: "/", // 👈 IMPORTANT
         display: "standalone",
+        background_color: "#ffffff", // 👈 IMPORTANT
         theme_color: "#2563eb",
         icons: [
-          { src: "/icon-192.png", sizes: "192x192", type: "image/png" },
-          { src: "/icon-512.png", sizes: "512x512", type: "image/png" },
+          {
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
         ],
       },
     }),
